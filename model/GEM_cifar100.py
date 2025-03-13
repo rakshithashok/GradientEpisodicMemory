3443eda24cdb8c0404ccc6bbae5b0b69a697b39b
rakshithashok <91626078+rakshithashok@users.noreply.github.com>
2025-03-13 18:43:45 +0530

    Create GEM_cifar100.py

---
+++ model/GEM_cifar100.py
@@ -0,0 +1,134 @@
+# Ensure import statement includes torch.cuda
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import quadprog
+from .common import MLP, ResNet18
+
+# Modify the initialization of memory_data to match the shape of input data
+class Net(nn.Module):
+    def __init__(self,
+                 n_inputs,
+                 n_outputs,
+                 n_tasks,
+                 args):
+        super(Net, self).__init__()
+        nl, nh = args.n_layers, args.n_hiddens
+        self.margin = args.memory_strength
+        self.is_cifar = (args.data_file == 'cifar100.pt')
+        if self.is_cifar:
+            self.net = ResNet18(n_outputs)
+        else:
+            self.net = MLP([n_inputs] + [nh] * nl + [n_outputs])
+
+        self.ce = nn.CrossEntropyLoss()
+        self.n_outputs = n_outputs
+
+        self.opt = optim.SGD(self.parameters(), args.lr)
+
+        self.n_memories = args.n_memories
+        self.gpu = args.cuda
+
+        # allocate episodic memory
+        if self.is_cifar:
+            self.memory_data = torch.FloatTensor(
+                n_tasks, self.n_memories, 3, 32, 32)
+        else:
+            self.memory_data = torch.FloatTensor(
+                n_tasks, self.n_memories, n_inputs)
+        self.memory_labs = torch.LongTensor(n_tasks, self.n_memories)
+        if args.cuda:
+            self.memory_data = self.memory_data.cuda()
+            self.memory_labs = self.memory_labs.cuda()
+
+        # allocate temporary synaptic memory
+        self.grad_dims = []
+        for param in self.parameters():
+            self.grad_dims.append(param.data.numel())
+        self.grads = torch.Tensor(sum(self.grad_dims), n_tasks)
+        if args.cuda:
+            self.grads = self.grads.cuda()
+
+        # allocate counters
+        self.observed_tasks = []
+        self.old_task = -1
+        self.mem_cnt = 0
+        if self.is_cifar:
+            self.nc_per_task = int(n_outputs / n_tasks)
+        else:
+            self.nc_per_task = n_outputs
+
+    def forward(self, x, t):
+        output = self.net(x)
+        if self.is_cifar:
+            # make sure we predict classes within the current task
+            offset1 = int(t * self.nc_per_task)
+            offset2 = int((t + 1) * self.nc_per_task)
+            if offset1 > 0:
+                output[:, :offset1].data.fill_(-10e10)
+            if offset2 < self.n_outputs:
+                output[:, offset2:self.n_outputs].data.fill_(-10e10)
+        return output
+
+    def observe(self, x, t, y):
+        # update memory
+        if t != self.old_task:
+            self.observed_tasks.append(t)
+            self.old_task = t
+
+        # Update ring buffer storing examples from current task
+        bsz = y.data.size(0)
+        endcnt = min(self.mem_cnt + bsz, self.n_memories)
+        effbsz = endcnt - self.mem_cnt
+        self.memory_data[t, self.mem_cnt: endcnt].copy_(
+            x.data[: effbsz])
+        if bsz == 1:
+            self.memory_labs[t, self.mem_cnt] = y.data[0]
+        else:
+            self.memory_labs[t, self.mem_cnt: endcnt].copy_(
+                y.data[: effbsz])
+        self.mem_cnt += effbsz
+        if self.mem_cnt == self.n_memories:
+            self.mem_cnt = 0
+
+        # compute gradient on previous tasks
+        if len(self.observed_tasks) > 1:
+            for tt in range(len(self.observed_tasks) - 1):
+                self.zero_grad()
+                # fwd/bwd on the examples in the memory
+                past_task = self.observed_tasks[tt]
+
+                offset1, offset2 = compute_offsets(past_task, self.nc_per_task,
+                                                   self.is_cifar)
+                ptloss = self.ce(
+                    self.forward(
+                        self.memory_data[past_task],
+                        past_task)[:, offset1: offset2],
+                    self.memory_labs[past_task] - offset1)
+                ptloss.backward()
+                store_grad(self.parameters, self.grads, self.grad_dims,
+                           past_task)
+
+        # now compute the grad on the current minibatch
+        self.zero_grad()
+
+        offset1, offset2 = compute_offsets(t, self.nc_per_task, self.is_cifar)
+        loss = self.ce(self.forward(x, t)[:, offset1: offset2], y - offset1)
+        loss.backward()
+
+        # check if gradient violates constraints
+        if len(self.observed_tasks) > 1:
+            # copy gradient
+            store_grad(self.parameters, self.grads, self.grad_dims, t)
+            indx = torch.cuda.LongTensor(self.observed_tasks[:-1]) if self.gpu \
+                else torch.LongTensor(self.observed_tasks[:-1])
+            dotp = torch.mm(self.grads[:, t].unsqueeze(0),
+                            self.grads.index_select(1, indx))
+            if (dotp < 0).sum() != 0:
+                project2cone2(self.grads[:, t].unsqueeze(1),
+                              self.grads.index_select(1, indx), self.margin)
+                # copy gradients back
+                overwrite_grad(self.parameters, self.grads[:, t],
+                               self.grad_dims)
+        self.opt.step()
